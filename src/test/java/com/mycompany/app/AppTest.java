a47d969d39a96505c088d915ab6444cf41b15cfe
Rochak Agrawal <36627691+rochakagrawal89@users.noreply.github.com>
2026-01-04 12:19:39 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,48 +1,12 @@
 package com.mycompany.app;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.After;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-{
-
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
+public class AppTest {
 
     @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-        } catch (Exception e) {
-            fail("Construction failed.");
-        }
+    public void testAppMain() {
+        assertEquals("Hello World!", App.mainMessage());
     }
-
-    @Test
-    public void testAppMain()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
-
 }
